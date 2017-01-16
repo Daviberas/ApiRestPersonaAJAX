dd49ac77885f27e0aa46e055cddf67dcdebc50be
David <daviberas6@gmail.com>
2017-01-16 06:49:33 +0100

    fredgr

--- PruebaApiREST/src/PruebaApiREST/wwwroot/Scripts/JavaScript.js
+++ PruebaApiREST/src/PruebaApiREST/wwwroot/Scripts/JavaScript.js
@@ -3,11 +3,12 @@
 function start()
 {
     document.getElementById("btnListar").addEventListener("click", listar);
-    document.getElementById("btnInsertar").addEventListener("click", insertar);
+    document.getElementById("btnInsertar").addEventListener("click", mostrarFormulario);
     document.getElementById("btnBorrar").addEventListener("click", borrar);
 }
 
-function listar() {
+function listar()
+{
     //1. Instanciar objeto XMLHttpRequest
     var json = new XMLHttpRequest();
 
@@ -42,84 +43,180 @@ function listar() {
     json.send();
 }
 
-function escribirPersonas(arrayPersonas)
+function mostrarFormulario()
 {
-    var table = document.createElement("TABLE");
-    table.setAttribute("border", "1");
-    table.setAttribute("cellspacing", "0");
-    var fila = document.createElement("TR");
-    var columna = document.createElement("TH");
+    var parraf = document.createElement("p");
     var texto;
+    var input;
     texto = document.createTextNode("Nombre");
-    columna.appendChild(texto);
-    fila.appendChild(columna);
+    parraf.appendChild(texto);
+    input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "txtNombre");
+    parraf.appendChild(input);
+    document.getElementById("formularioPersona").appendChild(parraf);
 
-    columna = document.createElement("TH");
+    parraf = document.createElement("p");
     texto = document.createTextNode("Apellidos");
-    columna.appendChild(texto);
-    fila.appendChild(columna);
+    parraf.appendChild(texto);
+    input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "txtApellidos");
+    parraf.appendChild(input);
+    document.getElementById("formularioPersona").appendChild(parraf);
 
-    columna = document.createElement("TH");
+    parraf = document.createElement("p");
     texto = document.createTextNode("Fecha de nacimiento");
-    columna.appendChild(texto);
-    fila.appendChild(columna);
+    parraf.appendChild(texto);
+    input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "txtFechaNac");
+    parraf.appendChild(input);
+    document.getElementById("formularioPersona").appendChild(parraf);
 
-    columna = document.createElement("TH");
+    parraf = document.createElement("p");
     texto = document.createTextNode("Teléfono");
-    columna.appendChild(texto);
-    fila.appendChild(columna);
+    parraf.appendChild(texto);
+    input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "txtTelefono");
+    parraf.appendChild(input);
+    document.getElementById("formularioPersona").appendChild(parraf);
 
-    columna = document.createElement("TH");
-    texto = document.createTextNode("Dirección");
-    columna.appendChild(texto);
-    fila.appendChild(columna);
-    
-    table.appendChild(fila);
+    parraf = document.createElement("p");
+    texto = document.createTextNode("Direccion");
+    parraf.appendChild(texto);
+    input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "txtDireccion");
+    parraf.appendChild(input);
+    document.getElementById("formularioPersona").appendChild(parraf);
 
-    for (i = 0; i < arrayPersonas.length; i++)
-    {
-        var persona = new Persona(arrayPersonas[i].nombre,arrayPersonas[i].apellidos,arrayPersonas[i].fechaNac,arrayPersonas[i].telefono,arrayPersonas[i].direccion);
-        fila = document.createElement("TR");
-        columna = document.createElement("TD");
-        texto = document.createTextNode(persona.nombre);
-        columna.appendChild(texto);
-        fila.appendChild(columna);
+    var boton = document.createElement("input");
+    boton.setAttribute("type", "button");
+    boton.setAttribute("id", "btnGuardar");
+    boton.setAttribute("value", "Guardar");
+    document.getElementById("formularioPersona").appendChild(boton);
 
-        columna = document.createElement("TD");
-        texto = document.createTextNode(persona.apellidos);
-        columna.appendChild(texto);
-        fila.appendChild(columna);
-
-        columna = document.createElement("TD");
-        texto = document.createTextNode(persona.fechaNac);
-        columna.appendChild(texto);
-        fila.appendChild(columna);
-
-        columna = document.createElement("TD");
-        texto = document.createTextNode(persona.telefono);
-        columna.appendChild(texto);
-        fila.appendChild(columna);
-
-        columna = document.createElement("TD");
-        texto = document.createTextNode(persona.direccion);
-        columna.appendChild(texto);
-        fila.appendChild(columna);
-
-        table.appendChild(fila);
-    }
-    document.getElementById("contenedorListaPersonas").innerHTML = "";
-
-    document.getElementById("contenedorListaPersonas").appendChild(table);
+    //boton.addEventListener("click", insertar);
 }
 
-class Persona
+function borrar()
 {
-    constructor(nombre,apellidos,fechaNac,telefono,direccion)
+    if (document.getElementById("txtIDaBorrar").innerHTML != " " && document.getElementById("txtIDaBorrar").innerHTML != null)
     {
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.fechaNac = fechaNac;
-        this.telefono = telefono;
-        this.direccion = direccion;
+        var json = new XMLHttpRequest();
+
+        json.open("DELETE", "../api/persona" + "/" + document.getElementById("txtIDaBorrar").innerText);
+
+        
+        json.onreadystatechange = function ()
+        {
+            if (json.readyState < 4)
+            {
+                document.getElementById("contenedorListaPersonas").innerHTML = "Cargando...";
+            }
+            else
+                if (json.readyState == 4 && json.status == 200)
+                {
+                    
+                }
+        }
+
+        json.send();
+        listar();
     }
 }
+
+    function escribirPersonas(arrayPersonas)
+    {
+        var table = document.createElement("TABLE");
+        table.setAttribute("class","tablaPersonas");
+        var fila = document.createElement("TR");
+        var columna = document.createElement("TH");
+        var texto;
+        texto = document.createTextNode("ID");
+        columna.appendChild(texto);
+        fila.appendChild(columna);
+
+        columna = document.createElement("TH");
+        texto = document.createTextNode("Nombre");
+        columna.appendChild(texto);
+        fila.appendChild(columna);
+
+        columna = document.createElement("TH");
+        texto = document.createTextNode("Apellidos");
+        columna.appendChild(texto);
+        fila.appendChild(columna);
+
+        columna = document.createElement("TH");
+        texto = document.createTextNode("Fecha de nacimiento");
+        columna.appendChild(texto);
+        fila.appendChild(columna);
+
+        columna = document.createElement("TH");
+        texto = document.createTextNode("Teléfono");
+        columna.appendChild(texto);
+        fila.appendChild(columna);
+
+        columna = document.createElement("TH");
+        texto = document.createTextNode("Dirección");
+        columna.appendChild(texto);
+        fila.appendChild(columna);
+    
+        table.appendChild(fila);
+
+        for (i = 0; i < arrayPersonas.length; i++)
+        {
+            var persona = new Persona(arrayPersonas[i].id,arrayPersonas[i].nombre,arrayPersonas[i].apellidos,arrayPersonas[i].fechaNac,arrayPersonas[i].telefono,arrayPersonas[i].direccion);
+            fila = document.createElement("TR");
+            columna = document.createElement("TD");
+            texto = document.createTextNode(persona.id);
+            columna.appendChild(texto);
+            fila.appendChild(columna);
+
+            columna = document.createElement("TD");
+            texto = document.createTextNode(persona.nombre);
+            columna.appendChild(texto);
+            fila.appendChild(columna);
+
+            columna = document.createElement("TD");
+            texto = document.createTextNode(persona.apellidos);
+            columna.appendChild(texto);
+            fila.appendChild(columna);
+
+            columna = document.createElement("TD");
+            texto = document.createTextNode(persona.fechaNac);
+            columna.appendChild(texto);
+            fila.appendChild(columna);
+
+            columna = document.createElement("TD");
+            texto = document.createTextNode(persona.telefono);
+            columna.appendChild(texto);
+            fila.appendChild(columna);
+
+            columna = document.createElement("TD");
+            texto = document.createTextNode(persona.direccion);
+            columna.appendChild(texto);
+            fila.appendChild(columna);
+
+            table.appendChild(fila);
+        }
+
+        document.getElementById("contenedorListaPersonas").innerHTML = "";
+
+        document.getElementById("contenedorListaPersonas").appendChild(table);
+    }
+
+    class Persona
+    {
+        constructor(id, nombre, apellidos, fechaNac, telefono, direccion)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.fechaNac = fechaNac;
+            this.telefono = telefono;
+            this.direccion = direccion;
+        }
+    }
